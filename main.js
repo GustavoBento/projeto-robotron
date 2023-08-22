f307ac2fc37423d186c20e44ea6d09890f0c5ddf
Gustavo Bento <133028031+GustavoBento@users.noreply.github.com>
2023-08-22 20:20:55 -0300

    aplicando js no controle

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const robotron = document.querySelector(".robotron");
-robotron.addEventListener("click", dizOi)
+const controle = document.querySelectorAll('.controle-ajuste')
 
-function dizOi() {
-    console.log('oi')
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+    })
+})
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector(".controle-contador")
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) -1
+    } else {
+        peca.value = parseInt(peca.value) +1
+    }
 }
